b35f7ee338051ece6d0a1af43e812813c9b4cf40
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-03-14 20:05:50 +0100

    Add public path to env for gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  chainWebpack: (config) => {
+  publicPath: process.env.VUE_PUBLIC_PATH ? process.env.VUE_PUBLIC_PATH : '',
+  chainWebpack: config => {
     config.module
       .rule('vue')
       .use('vue-svg-inline-loader')
       .loader('vue-svg-inline-loader')
-      .options({ /* ... */ });
+      .options({
+        /* ... */
+      });
   },
 };
