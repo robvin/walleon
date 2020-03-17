251bfc94d804bdb66b73f49d74de7cbc56cac12d
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-03-17 22:36:53 +0100

    Update server public path.

--- walleon/server/src/middleware/serve.js
+++ walleon/server/src/middleware/serve.js
@@ -3,9 +3,9 @@ import serve from "koa-static";
 import path from "path";
 
 export const serveViews = () => {
-  return views(path.join(__dirname, "/../public"), {});
+  return views(path.join(__dirname, "/../../../client/dist"), {});
 };
 
 export const serveStatic = () => {
-  return serve(path.join(__dirname, "/../public"), {});
+  return serve(path.join(__dirname, "/../../../client/dist"), {});
 };
