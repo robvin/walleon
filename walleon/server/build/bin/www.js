fabf65929fcb67d782c586df55a6d22066665cd1
rabinage <me@robinhansson.se>
2020-03-17 21:36:22 +0100

    Remove build dir

--- walleon/server/build/bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _server = require("../lib/server");
-
-(0, _server.createServer)().then(app => app.listen(8080, () => {}), err => {
-  console.log(err);
-  process.exit(1);
-});
-//# sourceMappingURL=www.js.map
