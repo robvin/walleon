c3bead984b9d7d7b4a84327cbdeb3d40230c1da9
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-03-20 14:52:24 +0100

    Add Switch device

---
+++ walleon/client/src/components/devices/Switch.vue
@@ -0,0 +1,58 @@
+<template>
+  <button class="device" :class="classes" @click="toggle()" :disabled="loading">
+    <div class="device__icon" :class="{ 'bg-green': active }">
+      <img svg-inline src="@/assets/images/plug.svg" />
+    </div>
+    <div class="device__info">
+      <span class="device__room">{{ device.domain }}</span>
+      <span class="device__name">{{ device.name }}</span>
+      <span class="device__state">{{ device.state }}</span>
+    </div>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Device as DeviceType } from "@/types";
+
+export default Vue.extend({
+  name: "LightDevice",
+
+  props: {
+    device: {
+      type: Object as () => DeviceType,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      loading: false
+    };
+  },
+
+  computed: {
+    active(): boolean {
+      return this.device.state === "on";
+    },
+
+    classes(): object {
+      return {
+        "device--loading": this.loading,
+        "device--active": this.active
+      };
+    }
+  },
+
+  methods: {
+    toggle() {
+      this.loading = true;
+
+      setTimeout(() => {
+        this.loading = false;
+        this.device.state = this.active ? "off" : "on";
+      }, Math.random() * 3000);
+    }
+  }
+});
+</script>
--- walleon/client/src/util/ha.ts
+++ walleon/client/src/util/ha.ts
@@ -19,6 +19,21 @@ export default {
           assumedState: true,
           unitOfMeasurement: "°C"
         }
+      },
+      {
+        state: "on",
+        entityId: "switch.kitchen",
+        domain: "switch",
+        objectId: "kitchen",
+        name: "Kitchen Switch",
+        lastUpdated: "2017-10-28 08:13:36.715874+00:00",
+        lastChanged: "2017-10-28 08:13:36.715874+00:00",
+        attributes: {
+          friendlyName: "Kitchen Switch",
+          icon: "mdi:home",
+          entityPicture: "http://example.com/picture.jpg",
+          assumedState: true,
+        }
       }
     ];
 
